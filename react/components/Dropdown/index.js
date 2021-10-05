22bb0af828e013d7c0e31b44545dab2cc5f04d49
deepi1997 <dreadeepika0@gmail.com>
2021-10-05 11:50:16 +0530

    srp-changes

--- react/components/Dropdown/index.js
+++ react/components/Dropdown/index.js
@@ -14,18 +14,24 @@ const DropdownButtonWrapper=() => {
         { value: 'amex', label: 'American Express' },*/
       ]
     return (
-        <div className="cf-ns nl2 nr2">
-       {/* <label>Show</label> */}
+        <div class="cf-ns nl2 nr2">
+          <div className="fl-ns w-25-ns ph2" style={ {"padding-top":"8px;", "text-align":" end;"} }>Show</div>
+
+      <div className="fl-ns w-50-ns ph2">
     <Dropdown
-     label="Show"
-      size="large"
+     //label="Show"
+      size="small"
       options={options}
       value={initialState.selected1}
       onChange={(_, v) => setState({ selected1: v })}
     />
+    </div>
     </div>  
        
     )
 }
 
 export default DropdownButtonWrapper
+
+//margin-left: -56px;
+  //  margin-bottom: -32px;
