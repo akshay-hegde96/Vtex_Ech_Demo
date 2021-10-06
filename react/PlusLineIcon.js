f6008622224c82ceab8c3d876e62eda3ca06198e
deepi1997 <dreadeepika0@gmail.com>
2021-10-06 12:12:43 +0530

    search page changes

---
+++ react/PlusLineIcon.js
@@ -0,0 +1,19 @@
+/*import React from 'react'
+
+import { Icon } from 'vtex.store-icons'
+
+  const IconSearch = () => {
+    const YourComponent = (props) => <Icon id="mpa-plus--line" />
+   return (
+       <div>
+           
+       </div>
+   )
+  }
+  
+ 
+  export default IconSearch*/
+
+
+ 
+
