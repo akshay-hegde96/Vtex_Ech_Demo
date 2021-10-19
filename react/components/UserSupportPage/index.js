17149e46e3b26f4f2cb5db6959829a7a154d54eb
reena-p <reena.p@echidnainc.com>
2021-10-19 18:31:00 +0530

    Updated my accounts page with the following:
    1. Added new fields in the profile section.
    2. Created new page, User Support.

---
+++ react/components/UserSupportPage/index.js
@@ -0,0 +1,14 @@
+import styles from "./UserSupportPage.css";
+
+const UserSupport = () => {
+  return (
+    <div className={styles.support}>
+      <h1>User Support</h1>
+      <div>How can I help you today?</div>
+    </div>
+  );
+};
+
+export default UserSupport;
+
+//style={{ marginLeft: "3rem", paddingTop: "2rem" }}
