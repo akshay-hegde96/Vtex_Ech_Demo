207f8e8293c34891d9d5802e9fa6f40f0cc0b2b4
Anubhav P <anubhav.p@echidnainc.com>
2021-10-29 18:33:41 +0530

    search bar UI update

---
+++ react/RemoveButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { IconClose } from 'vtex.store-icons'
+
+
+const RemoveButton = (props) => {
+    return (
+        <IconClose size="20"/>
+    )
+}
+
+export default RemoveButton
--- react/SearchButton.js
+++ react/SearchButton.js
@@ -1,15 +1,10 @@
 import React from "react"
-import { Collapsible } from 'vtex.styleguide'
-import { ButtonWithIcon } from 'vtex.styleguide'
 import { IconSearch } from 'vtex.store-icons'
 
 
 const SearchButton = (props) => {
-    ///initialState = { isOpen: false };
     return (
-        <ButtonWithIcon variation="secondary">
-            <IconSearch />
-        </ButtonWithIcon>
+        <IconSearch size="20"/>
     )
 }
 
