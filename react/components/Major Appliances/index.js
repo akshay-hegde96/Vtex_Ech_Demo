6926a5130d174dafcba97854130775612d445d44
Mansi Dhingani <mansi.vd@echidnainc.com>
2021-11-03 15:03:08 +0530

    Created makeAPICall function in httpCall.js file for all API fetch request. This function api can be used in all the module for all type of fetch API request.

--- react/components/Major Appliances/index.js
+++ react/components/Major Appliances/index.js
@@ -1,31 +1,40 @@
-import React from "react";
-import { majorAppliances } from "../Electronics/data";
+import React, { useState, useEffect } from "react";
 import styles from "../Electronics/Electronics.css";
 import homeIcon from "../../../assets/breadcrumb.png";
-import { storeURL } from "../../Conflicts/url";
+import { storeURL, deparmentURL } from "../../Conflicts/url";
+import { makeAPICall } from "../../utills/httpCall"
 
-const MajorAppliances = () => {
+const MajorAppliances = ({ }) => {
+
+  let data;
+  const [majorAppliancesDatas, setmajorAppliancesDatas] = useState([]);
+
+  useEffect(() => {
+      const makeMajorAppliancesAPICall = async () => {
+      data = await makeAPICall(deparmentURL, 'GET');
+      console.log({ data });
+      setmajorAppliancesDatas([...data]);
+    };
+
+    makeMajorAppliancesAPICall();
+  }, []);
+
   return (
     <React.Fragment>
       <div style={{ display: "flex" }}>
         <a href={storeURL}>
-          <img src={homeIcon} alt="home-icon"  className={styles.breadcrumbIcon} />
+          <img src={homeIcon} alt="home-icon" className={styles.breadcrumbIcon} />
         </a>
-        {/* <p style={{paddingTop:"16px",color:"#dadada"}}>Electronics</p> */}
       </div>
       <h2 className={styles.textAlignment}>Major Appliances</h2>
       <div className={styles.row}>
-        {majorAppliances.map((majorAppliance) => {
+        {majorAppliancesDatas.filter(majorAppliancesDatas => majorAppliancesDatas.id === 101).map((majorAppliance) => {
           return (
-            <div key={majorAppliance.id} className={styles.imgPadding}>
-              <a href={majorAppliance.url}>
-                <img
-                  src={majorAppliance.img}
-                  alt="department image"
-                  className={styles.imgSize}
-                />
-                <h2 className={styles.textAlignment}>{majorAppliance.name}</h2>
-              </a>
+            <div key={majorAppliance.id}>
+              <h2>{majorAppliance.name}</h2>
+              {majorAppliance.children.map(ak => {
+                return <h2>{ak.name}</h2>
+              })}
             </div>
           );
         })}
