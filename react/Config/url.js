b61a0cfef9b27abc4a795ebdfe29b4d54443329f
reena-p <reena.p@echidnainc.com>
2021-11-15 18:44:08 +0530

    Created Dynamic menu items

--- react/Config/url.js
+++ react/Config/url.js
@@ -5,5 +5,14 @@ export const storeURL = "https://development--echidna.myvtex.com";
 // category tree API
 export const deparmentURL = "api/catalog_system/pub/category/tree/10";
 
+// contact Us form data API
 export const contactUsURL =
   "api/dataentities/client/search?_fields=_all&_schema=person";
+
+//Menu Item API
+export const menuItemURL =
+  "api/dataentities/MenuItems/search?_schema=menuItem&_fields=title,menuId";
+
+//Sub Item API
+export const subItemURL =
+  "api/dataentities/subItem/search?_schema=subItems&_fields=item,menuId";
--- react/components/WomenMenuItems/index.js
+++ react/components/WomenMenuItems/index.js
@@ -1,52 +1,117 @@
-import React, { Component } from 'react'
-import styles from './WomenMenu.css'
+import React, { Component } from "react";
+import { menuItemURL, subItemURL } from "../.././Config/url";
+import { makeAPICall } from "../../Utils/httpCall";
+import styles from "./WomenMenu.css";
 
-const tiles = [
-    { title: "Clothing-Ethnic Wear", subItem: ["Sarees", "Kurtas & Kurtis", "Dress Material", "Lehenga Choli"] },
-    { title: "Clothing-Western Wear", subItem: ["Topwear", "Dresses", "Jeans", "Shorts", "Skirts"] },
-    { title: "Footwear-Sandals", subItem: ["Flats", "Heels", "Wedges"] },
-    { title: "Footwear-Shoes", subItem: ["Sports Shoes", "Casual Shoes", "Boots"] },
-    { title: "Personal Care Appliances", subItem: ["Hair Straightners", "Hair Dryers", "Epilators"] },
-    { title: "Beauty & Grooming", subItem: ["Make Up", "Skin Care", "Hair Care", "Deodorants & Perfumes"] },
-    { title: "Watch", subItem: ["Smart Watches", "Analog", "Watch Accessories"] },
-    { title: "Jewellery", subItem: ["Artificial Jewellery", "Silver Jewellery", "Precious Jewellery"] },
-    { title: "Accessories", subItem: ["Handbags", "Shoulder Bags", "Totes", "Sling bags", "Wallets & Belts", "Sunglasses"] },
-    { title: "Featured", subItem: ["Forever 21", "Chemistry", "Lakme", "Catwalk", "Titan-Raga", "Anmi", "Rare Roots"] },
-];
+// const tiles = [
+//   {
+//     title: "Clothing-Ethnic Wear",
+//     subItem: ["Sarees", "Kurtas & Kurtis", "Dress Material", "Lehenga Choli"],
+//   },
+//   {
+//     title: "Clothing-Western Wear",
+//     subItem: ["Topwear", "Dresses", "Jeans", "Shorts", "Skirts"],
+//   },
+//   { title: "Footwear-Sandals", subItem: ["Flats", "Heels", "Wedges"] },
+//   {
+//     title: "Footwear-Shoes",
+//     subItem: ["Sports Shoes", "Casual Shoes", "Boots"],
+//   },
+//   {
+//     title: "Personal Care Appliances",
+//     subItem: ["Hair Straightners", "Hair Dryers", "Epilators"],
+//   },
+//   {
+//     title: "Beauty & Grooming",
+//     subItem: ["Make Up", "Skin Care", "Hair Care", "Deodorants & Perfumes"],
+//   },
+//   { title: "Watch", subItem: ["Smart Watches", "Analog", "Watch Accessories"] },
+//   {
+//     title: "Jewellery",
+//     subItem: ["Artificial Jewellery", "Silver Jewellery", "Precious Jewellery"],
+//   },
+//   {
+//     title: "Accessories",
+//     subItem: [
+//       "Handbags",
+//       "Shoulder Bags",
+//       "Totes",
+//       "Sling bags",
+//       "Wallets & Belts",
+//       "Sunglasses",
+//     ],
+//   },
+//   {
+//     title: "Featured",
+//     subItem: [
+//       "Forever 21",
+//       "Chemistry",
+//       "Lakme",
+//       "Catwalk",
+//       "Titan-Raga",
+//       "Anmi",
+//       "Rare Roots",
+//     ],
+//   },
+// ];
 
 export class Womenmenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuItemData: [],
+      subItemData: [],
+    };
+  }
+  componentDidMount() {
+    const findElem = document.querySelector(
+      ".vtex-store-drawer-0-x-drawerContent"
+    );
+    findElem &&
+      findElem.setAttribute(
+        "style",
+        "overflow-y:scroll; height:-webkit-fill-available;"
+      );
+    const menuExp = document.querySelectorAll(
+      ".vtex-menu-2-x-accordionIcon--megaMenu-header"
+    );
+    menuExp.forEach((item) => item.setAttribute("style", "color:darkgreen"));
 
-    componentDidMount() {
-        const findElem = document.querySelector('.vtex-store-drawer-0-x-drawerContent');
-        findElem && findElem.setAttribute('style', 'overflow-y:scroll; height:-webkit-fill-available;');
-        const menuExp = document.querySelectorAll('.vtex-menu-2-x-accordionIcon--megaMenu-header');
-        menuExp.forEach(item =>
-            item.setAttribute('style', 'color:darkgreen')
-        )
-    }
-    render() {
-        return (
-            <div className={styles.responsiveGrid}>
-                {
-                    tiles.map((tile, i) =>
-                        <div key={i} className={styles.tileData}>
-                            <div className={styles.tile}>
-                                <div className={styles.title}>{tile.title}</div>
-                                <div className={styles.text}>
-                                    {
-                                        tile.subItem.map((type, i) =>
-                                            <div key={i}>
-                                                <a className={styles.subTileData} href={type}>{type}</a>
-                                            </div>)
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
+    makeAPICall(menuItemURL, "GET").then((data) => {
+      console.log(data);
+      this.setState({ menuItemData: data });
+    });
+
+    makeAPICall(subItemURL, "GET").then((data) => {
+      console.log(data);
+      this.setState({ subItemData: data });
+    });
+  }
+  render() {
+    return (
+      <div className={styles.responsiveGrid}>
+        {this.state.menuItemData.map((eachTitle, i) => (
+          <div key={i} className={styles.tileData}>
+            <div className={styles.tile}>
+              <div className={styles.title}>{eachTitle.title}</div>
+              <div className={styles.text}>
+                {this.state.subItemData.map((type, i) => {
+                  let element = eachTitle.menuId === type.menuId && (
+                    <div key={i}>
+                      <a className={styles.subTileData} href={type.item}>
+                        {type.item}
+                      </a>
+                    </div>
+                  );
+                  return element;
+                })}
+              </div>
             </div>
-        );
-    }
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Womenmenu
+export default Womenmenu;
