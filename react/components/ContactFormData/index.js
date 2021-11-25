17bd928ea13b74793a0b006119836a676d5bafcd
reena-p <reena.p@echidnainc.com>
2021-11-25 18:42:43 +0530

    Included Pagination to Contact Us History page

--- react/components/ContactFormData/index.js
+++ react/components/ContactFormData/index.js
@@ -1,121 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./ContactFormData.css";
+import Table from "./Table/Table";
 import { makeAPICall } from "../../Utils/httpCall";
 import { contactUsURL } from "../.././Config/url";
-import styles from "./ContactFormData.css";
 
 const ContactFormData = () => {
-  const [list, setList] = useState([]);
-  const [data, setData] = useState({
-    id: "",
-    firstName: "",
-    lastName: "",
-    address: { streetNumber: 0, streetAddress: "", streetType: "" },
-    createdBy_USER: { Login: "" },
-    agreement: false,
-  });
-  const [showdetails, setShowdetails] = useState(false);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const contactDataCall = async () => {
       const responseData = await makeAPICall(contactUsURL, "GET");
       // console.log({ responseData });
-      setList(responseData);
+      setData(responseData);
     };
 
     contactDataCall();
   }, []);
 
-  const showDetailsHandler = (userData) => {
-    //console.log(userData);
-    setData(userData);
-    setShowdetails(!showdetails);
-  };
-
-  const details = (
-    <div className={styles.detail}>
-      <p> Name : {`${data.firstName} ${data.lastName}`}</p>
-      <p>
-        Address :{" "}
-        {`street no. ${data.address.streetNumber}, ${data.address.streetAddress}, ${data.address.streetType} `}
-      </p>
-      <p>UserId : {data.id}</p>
-      <p>Email : {data.createdBy_USER.Login}</p>
-      <p>Agreement : {data.agreement ? "Yes" : "No"}</p>
-    </div>
-  );
-
-  const stylesInline = {
-    display: "block",
-    textDecorationLine: "none",
-    textAlign: "start",
-    marginTop: "2rem",
-    marginLeft: "3.5rem",
-    fontSize: "0.97rem",
-  };
-
   return (
-    <div style={{ marginBottom: "11rem" }}>
-      <a style={stylesInline} href="/contact-us">
-        ⬅ Back
-      </a>
+    <React.Fragment>
+      <div>
+        <a className={styles.back_link} href="/contact-us">
+          ⬅ Back
+        </a>
+      </div>
 
       <div className={styles.fd_title}>
         <h3>Contact Us History</h3>
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "start",
-          flexWrap: "wrap",
-        }}
-      >
-        <div className={styles.container2}>
-          {list
-            .filter((user, i) => {
-              return i <= list.length / 2 === true;
-            })
-            .map((user, i) => {
-              let userData = user;
-              return (
-                <div key={user.id} className={styles.cardContainer}>
-                  <div
-                    className={styles.card}
-                    onClick={() => showDetailsHandler(userData)}
-                  >
-                    <p
-                      style={{ margin: "13px" }}
-                    >{`${user.firstName} ${user.lastName}`}</p>
-                  </div>
-                  {showdetails && user.id === data.id && details}
-                </div>
-              );
-            })}
-        </div>
 
-        <div className={styles.container2}>
-          {list
-            .filter((user, i) => {
-              return i > list.length / 2 === true;
-            })
-            .map((user, i) => {
-              let userData = user;
-              return (
-                <div key={user.id} className={styles.cardContainer}>
-                  <div
-                    className={styles.card}
-                    onClick={() => showDetailsHandler(userData)}
-                  >
-                    <p
-                      style={{ margin: "13px" }}
-                    >{`${user.firstName} ${user.lastName}`}</p>
-                  </div>
-                  {showdetails && user.id === data.id && details}
-                </div>
-              );
-            })}
+      <main className={styles.container}>
+        <div className={styles.wrapper}>
+          <Table data={data} rowsPerPage={6} />
         </div>
-      </div>
-    </div>
+      </main>
+    </React.Fragment>
   );
 };
 
