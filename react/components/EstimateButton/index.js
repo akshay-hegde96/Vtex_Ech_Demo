2c67801e17dc01ef155789de7e98f2bf53ba86ea
Deepika <deepika@ECHIDNA.COM>
2021-12-09 11:23:31 +0530

    changes made in pdp

--- react/components/EstimateButton/index.js
+++ react/components/EstimateButton/index.js
@@ -1,17 +1,46 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 import styles from "./estimateBtn.css";
 
 
-export class index extends Component {
-    render() {
-        return (
-            <div>
-                <span id="btnestimateShippingWrapper" className={styles.btnestimateShippingWrapper} >
-<input type="submit" name="cestimateShipping" value="ESTIMATE COST" id="cestimateShipping" className={styles.cestimateShipping} />
-</span>
-            </div>
-        )
-    }
-}
+// function Toggle() {
+    /*const [show, setShow] = useState(false);
 
-export default index
+    return (
+        <div>
+                <span id="btnestimateShippingWrapper" className={styles.btnestimateShippingWrapper} >
+<input type="submit" onClick={()=>setShow()}  name="cestimateShipping" value="ESTIMATE COST" id="cestimateShipping" className={styles.cestimateShipping} />
+</span>
+<span>
+
+</span>
+        </div>
+    )
+}*/
+const Toggle = () => {
+    const [show, setShow] = useState(false);
+    const [hidebtn , setHidebtn] = useState(true);
+    const [loader , setLoader] = useState(false);
+
+    const toggleShow=()=>{
+        setLoader(true);
+        setTimeout(()=>{
+            setShow(true);
+            setHidebtn(false);
+            setLoader(false);
+        },2000) 
+    }
+
+    return (
+      <div>
+          {hidebtn && !loader && <button className={styles.cestimateShipping} onClick={toggleShow }>ESTIMATE COST</button> }
+           
+        {show &&  !loader &&  <div className={styles.text}>Free</div>}
+        {loader &&  <div className={styles.spinner}></div>}
+      </div>
+    )
+  }
+  
+
+
+export default Toggle
+
