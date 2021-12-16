86e7a167b19d47a1e00a8089ceb334c4399ce1a4
Deepika <deepika@ECHIDNA.COM>
2021-12-16 20:10:49 +0530

    footer changes for static pages

--- react/components/Dropdown/index.js
+++ react/components/Dropdown/index.js
@@ -3,21 +3,32 @@ import { Dropdown } from 'vtex.styleguide'
 
 const DropdownButtonWrapper=() => {
     const initialState = {
-        selected1: '1'
+        selected1: 'Select Model',
+        
       }
       const options = [
-        { value: '1', label: '1' },
-        { value: '2', label: '2' },
-        { value: '3', label: '3' },
-       /* { value: 'diners', label: 'Diners' },
-        { value: 'giftcard', label: 'Gift Card' },
-        { value: 'amex', label: 'American Express' },*/
+        { value: 'Altima', label: 'Altima' },
+        { value: 'Altima Couple', label: 'Altima Couple' },
+        { value: 'Armada', label: 'Armada' },
+       { value: 'JUKE', label: 'JUKE' },
+        { value: 'Cube', label: 'Cube' },
+        { value: 'Frontier', label: 'Frontier' }
       ]
+
+      const myoptions = [
+        { value: '2013', label: '2013' },
+        { value: '2014 Couple', label: '2014'},
+        { value: '2015', label: '2015' },
+        {value: '2016', label: '2016' },
+        { value: '2017', label: '2017' },
+        { value: '2018', label: '2018' }
+      ]
+
     return (
         <div class="cf-ns nl2 nr2">
-          <div className="fl-ns w-25-ns ph2" style={ {"padding-top":"8px;", "text-align":" end;"} }>Show</div>
+          <div className="fl-ns w-25-ns ph2" >MODEL</div>
 
-      <div className="fl-ns w-50-ns ph2">
+      <div className="fl-ns w-50-ns ph2" >
     <Dropdown
      //label="Show"
       size="small"
@@ -26,8 +37,19 @@ const DropdownButtonWrapper=() => {
       onChange={(_, v) => setState({ selected1: v })}
     />
     </div>
+
+    <div className="fl-ns w-50-ns ph2" >
+    <Dropdown
+     //label="Show"
+      size="small"
+      options={myoptions}
+      value={initialState.selected1}
+      onChange={(_, v) => setState({ selected1: v })}
+    />
+    </div>
+
     </div>  
-       
+      
     )
 }
 
@@ -35,3 +57,5 @@ export default DropdownButtonWrapper
 
 //margin-left: -56px;
   //  margin-bottom: -32px;
+
+  // style={ {"width":"230px;"} }
